419442a11f4382e6c7f54b0a572556629fbc47e8
TraderBOT-Suporte <trader.bot.83.28@gmail.com>
2026-03-23 05:21:10 +0100

    Delete js/config.js

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// js/config.local.js (NÃO enviar para o GitHub)
-const firebaseConfig = {
-    apiKey: "AIzaSyCIR3bs71W2zrKHn1xL6atAVoFvOYUheh8",
-    authDomain: "trader-bot-58966.firebaseapp.com",
-    projectId: "trader-bot-58966",
-    storageBucket: "trader-bot-58966.firebasestorage.app",
-    messagingSenderId: "694501488577",
-    appId: "1:694501488577:web:6456e9918d0305a742a140"
-};
